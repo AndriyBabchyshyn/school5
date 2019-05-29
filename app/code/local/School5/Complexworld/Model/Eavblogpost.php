8dbb40adab35e74fa2b2a1fe745ff314a9720124
AndriyBabchyshyn <andriy.babchyshyn@gmail.com>
2019-05-29 22:05:12 +0300

    All tasks within EAV

--- app/code/local/School5/Complexworld/Model/Eavblogpost.php
+++ app/code/local/School5/Complexworld/Model/Eavblogpost.php
@@ -2,6 +2,13 @@
 
 class School5_Complexworld_Model_Eavblogpost extends Mage_Core_Model_Abstract
 {
+    /**
+     * Prefix of model events names
+     *
+     * @var string
+     */
+    protected $_eventPrefix = 'eav_blog_post';
+
     protected function _construct()
     {
         $this->_init('complexworld/eavblogpost');
--- app/code/local/School5/TextCreator/Model/Blogposts.php
+++ app/code/local/School5/TextCreator/Model/Blogposts.php
@@ -2,6 +2,13 @@
 
 class School5_TextCreator_Model_Blogposts extends Mage_Core_Model_Abstract
 {
+    /**
+     * Prefix of model events names
+     *
+     * @var string
+     */
+    protected $_eventPrefix = 'school5_blog_post';
+
     protected function _construct()
     {
 
