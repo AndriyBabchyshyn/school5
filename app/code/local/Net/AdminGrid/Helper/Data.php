017392f21be7ad659d98eeff5ef391da2038dbb7
AndriyBabchyshyn <andriy.babchyshyn@gmail.com>
2019-06-10 21:35:42 +0300

    Custom Grid Added

---
+++ app/code/local/Net/AdminGrid/Helper/Data.php
@@ -0,0 +1,5 @@
+<?php
+class Net_AdminGrid_Helper_Data extends Mage_Core_Helper_Abstract
+{
+}
+	 
---
+++ app/code/local/Net/AdminGrid/controllers/Adminhtml/AdgridController.php
@@ -0,0 +1,16 @@
+<?php
+class Net_AdminGrid_Adminhtml_AdgridController extends Mage_Adminhtml_Controller_Action
+{
+
+	protected function _isAllowed()
+	{
+		return Mage::getSingleton('admin/session')->isAllowed('admingrid/adgrid');
+	}
+
+	public function indexAction()
+    {
+       $this->loadLayout();
+	   $this->_title($this->__("Admin Grid"));
+	   $this->renderLayout();
+    }
+}
